cc412918e2d7203cc9ec7c631e8d0b6a1bdcc435
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2026-09-17 18:55:44 -0500

    Create VAE architecture with sanity shape check

---
+++ src/vae.py
@@ -0,0 +1,75 @@
+import torch.nn as nn
+import numpy as np
+import torch
+
+torch.manual_seed(42)
+
+class encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc_mu = nn.Linear(128*6*6, 128)
+        self.fc_logvar = nn.Linear(128*6*6, 128)
+        self.flatten = nn.Flatten()
+        self.network = nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16,out_channels=32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32,out_channels=64, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,out_channels=128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.network(x)
+        x = self.flatten(x)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
+
+
+class decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(128, 128*6*6)
+        self.network = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=128,out_channels=64,kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=16,out_channels=3,kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc(x)
+        x = x.view(x.shape[0],128,6,6)
+        x_recon = self.network(x)
+        return x_recon
+
+class VAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = encoder()
+        self.decoder = decoder()
+
+    def reparametrize(self, mu, logvar):
+        return mu + torch.exp(0.5*logvar) * torch.randn_like(mu)
+
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparametrize(mu, logvar)
+        x_recon = self.decoder(z)
+        return mu, logvar, x_recon
+
+
+if __name__ == "__main__":
+    model = VAE()
+    sanity_check = torch.rand(4, 3, 96, 96)
+    mu, logvar, x_recon = model(sanity_check)
+    print(f"mu shape: {mu.shape}, logvar shape: {logvar.shape}, x_recon shape = {x_recon.shape}")    
