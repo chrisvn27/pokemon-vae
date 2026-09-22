2f644c07a95abac79815f30d4ef4a9476f923f96
chrisvn27 <180705575+chrisvn27@users.noreply.github.com>
2026-09-22 17:24:09 -0500

    Return three dataloaders for train, validation, and test

--- src/data_processing.py
+++ src/data_processing.py
@@ -33,17 +33,18 @@ transform = transforms.Compose([
 def convert_to_dataloader_train_and_test(batch_size = batch_size,transform = transform):
     records = build_records()
     dataset_records = ImageDataSet(records,transform)
-    dataset_records_train, dataset_records_test = random_split(dataset_records, [0.9, 0.1])
+    dataset_records_train, dataset_records_val, dataset_records_test = random_split(dataset_records, [0.8, 0.1, 0.1])
     dataloader_train = DataLoader(dataset_records_train, batch_size= batch_size, shuffle= True)
+    dataloader_val = DataLoader(dataset_records_val, batch_size= batch_size)
     dataloader_test = DataLoader(dataset_records_test, batch_size=batch_size)
 
-    return dataloader_train, dataloader_test
+    return dataloader_train, dataloader_val, dataloader_test
 
 
 
 if __name__ == "__main__":
 
-    train_dataloader , test_dataloader = convert_to_dataloader_train_and_test()
+    train_dataloader, val_dataloader, test_dataloader = convert_to_dataloader_train_and_test()
 
     first_batch_check = next(iter(train_dataloader))
     print("Number of images per batch: ", {len(first_batch_check)})
